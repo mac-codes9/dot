ae9b82ec91beec1b2d41c441d1d537947f5a2cff
Mac <maclong9@icloud.com>
2023-12-14 21:12:57 +0000

    fix(install): executable

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ if [ -d "$HOME/.termux" ]; then
 fi
 chsh -s $(which zsh)
 
-git clone https://github.com/mac-codes9/dot
+git clone https://github.com/mac-codes9/dot .
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
