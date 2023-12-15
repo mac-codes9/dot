9b23c5c24bc27bc4e7c86a99d71c6a74973a2988
Mac <maclong9@icloud.com>
2023-12-15 17:37:54 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 if [ -d "$HOME/.termux" ]; then
-  pkg update; pkg add yq git
+  pkg update; pkg upgrade; pkg add yq git
   rm -rf .termux
   git init .
   git remote add origin https://github.com/mac-codes9/dot
   git pull origin master
   yq e '.tools.common' config.yml | xargs -n1 pkg install -y
 fi
+
 chsh -s $(which zsh)
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
 zsh -c "source .zshrc; zplug install; zplug load"
 zsh -c "vim +PlugInstall +qall"
