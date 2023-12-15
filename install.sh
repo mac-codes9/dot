569c72c1af90e16d901fe16420952e0df7e3603c
Mac <maclong9@icloud.com>
2023-12-15 16:18:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 if [ -d "$HOME/.termux" ]; then
   pkg update; pkg add yq git
-  git clone https://github.com/mac-codes9/dot .
+  git clone --force https://github.com/mac-codes9/dot .
   yq e '.tools.common' config.yml | xargs -n1 pkg install -y
 fi
-# TODO: add commandn for mac and linux
+
 chsh -s $(which zsh)
 
-git clone -f https://github.com/mac-codes9/dot .
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 zsh -c "source ~/.zshrc; zplug install; zplug load"
