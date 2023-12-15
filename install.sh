2aa989febd5e49763bfd4a5412ef146bfc7ac260
Mac <maclong9@icloud.com>
2023-12-15 17:46:02 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ if [ -d "$HOME/.termux" ]; then
   git remote add origin https://github.com/mac-codes9/dot
   git pull origin master
   yq e '.tools.common' config.yml | xargs -n1 pkg install -y
+  pkg upgrade 
 fi
 
 chsh -s $(which zsh)
