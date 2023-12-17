12204a8e6b0d53855c5e44afaf48a8f2eaf0e7f0
Mac <maclong9@icloud.com>
2023-12-17 14:45:37 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-function clone_config() {
+clone_config() {
   cd
   git init .
   git remote add origin https://github.com/mac-codes9/dot
@@ -11,13 +11,13 @@ if [ -d "$HOME/.termux" ]; then
   yes | pkg upgrade
   pkg install -y yq git
   rm -rf ~/.termux
-  clone_config()
+  clone_config
   yq e '.tools.common' config.yml | xargs -n1 pkg install -y
   yes | pkg upgrade
 fi
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh;
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 sleep 2s
 
