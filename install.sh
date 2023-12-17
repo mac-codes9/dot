37e5a8533e57374c9c6f4a401f1e9e5f245664d8
Mac <maclong9@icloud.com>
2023-12-17 13:54:09 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,9 +11,9 @@ if [ -d "$HOME/.termux" ]; then
   yes | pkg upgrade
 fi
 
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
+
 
 zsh -c "source ~/.zshrc; zplug install; zplug load"
 zsh -c "vim +PlugInstall +qall"
