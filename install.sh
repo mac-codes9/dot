5ca3db517693af28954f9e5976f277ee0dbd5ce8
Mac <maclong9@icloud.com>
2023-12-17 16:21:11 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,8 +20,8 @@ fi
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 while [ ! -f ~/.zplug/init.zsh ]; do
-  sleep 50ms
+  sleep 0.5
 done
-zsh -c "source ~/.zplug/init.zsh && zplug install && zplug load"
+zsh -c "source ~/.zshrc && zplug install && zplug load"
 vim +PlugInstall +qall
 chsh -s zsh
