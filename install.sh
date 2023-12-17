800c3316c08d113099c7ac916997d8fe46851ad0
Mac <maclong9@icloud.com>
2023-12-17 18:33:11 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 clone_config() {
   cd
+  git config --global user.email $(yq e '.user.name' config.yml)
+  git config --global user.name $(yq e '.user.email' config.yml)
   git init .
   git remote add origin https://github.com/mac-codes9/dot
   git pull origin master 
@@ -18,16 +20,12 @@ if [ -d "$HOME/.termux" ]; then
 fi
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+vim +PlugInstall +qall
 
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+zsh -c "source ~/.zshrc && zplug update && zplug install && zplug load"
 while [ ! -f ~/.zplug/init.zsh -o ! -f ~/.zplug/cache/defer_3_plugin.zsh ]; do
   sleep 1
 done
-
 zsh -c "source ~/.zshrc && zplug update && zplug install && zplug load"
-vim +PlugInstall +qall
-
-git config --global user.email $(yq e '.user.name' config.yml)
-git config --global user.name $(yq e '.user.email' config.yml)
-
 chsh -s zsh
