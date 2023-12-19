b25ba5a72f562563edf4a3595d28fd1d022704d2
Mac <maclong9@icloud.com>
2023-12-19 11:36:30 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ if [ -d "$HOME/.termux" ]; then
   pkg install -y yq git
   rm -rf ~/.termux
   clone_config
-  yq e '.tools.all.common[]' config.yml | xargs pkg install -y
+  yq e '.tools.all.packages[]' config.yml | xargs pkg install -y
   yq e '.tools.all.node[]' config.yml | xargs npm install -g
   yes | pkg upgrade
 fi
