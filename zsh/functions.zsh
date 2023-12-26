f4fb6949d1b64f715f7426c4024b689e1c4503ed
maclong9@icloud.com <Mac>
2023-12-26 22:03:47 +0000

    feat(zc): +ghAliases ~sysCheckFunc -zce

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -6,13 +6,17 @@ mkcd() {
 system_check() {
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     if [[ -f /etc/arch-release ]]; then
+      echo -e "\e[1;32mRunning on Arch Linux\e[0m"
       os="arch"
     elif [[ -f /etc/lsb-release ]]; then
+      echo -e "\e[1;32mRunning on Ubuntu\e[0m"
       os="ubuntu"
     fi
   elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo -e "\e[1;32mRunning on macOS\e[0m"
     os="mac"
   elif [[ "$OSTYPE" == "linux-android"* ]]; then
+    echo -e "\e[1;32mRunning on Termux\e[0m"
     os="termux"
   fi
 }
