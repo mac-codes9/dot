81ae9f559858e3cfaef1f0b51a0f641cebdead33
Mac <maclong9@icloud.com>
2024-01-07 14:11:41 +0000

    feat(install): macOS script added

--- install.sh
+++ install.sh
@@ -1,38 +1,55 @@
 #!/bin/sh
+installer=brew
+tools=''
+if [ -d "$HOME/.termux" ]; then
+  echo "Running on Termux"
+  installer=pkg
+  tools = 'tools.all.packages[] + tools.all.zsh.packages[] + tools.all.node[] + tools.mobile.packages[] | join (" ")'
+elif [ "$(uname -s)" = "Darwin" ]; then
+  echo "Running on macOS"
+  installer=brew
+  tools='tools.all.packages[] + tools.all.zsh.packages[] + tools.all.node[] + tools.computer.all.packages[] + tools.computer.all.node[] + tools.computer.mac.packages[] | join(" ")'
+fi
+
 pre_install() {
-  $1 update
-  yes | $1 upgrade
-  $1 install -y yq git
-} 
+  if [ $installer -eq brew ]
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+
+  $installer update
+  $installer upgrade
+  $installer install -y yq git
+}
+
+post_install() {
+  mkdir docs
+  chsh -s zsh
+}
 
 clone_config() {
   cd && mkdir .config
   cd .config
-  git init .  
-  git remote add origin https://github.com/mac-codes9/dot  
+  git init .
+  git remote add origin https://github.com/mac-codes9/dot
   git pull origin master
   ln -s ~/.config/vim/vimrc ~/.vimrc
   ln -s ~/.config/zsh/zshrc ~/.zshrc
-  git config --global user.email $(yq e '.user.name' config.yml)
-  git config --global user.name $(yq e '.user.email' config.yml)
+  git config --global user.email $(yq e 'user.name' config.yml)
+  git config --global user.name $(yq e 'user.email' config.yml)
   git config --global push.autoSetupRemote true
   cd
   git clone https://github.com/mac-codes9/dot.wiki.git notes
 }
 
 install_tools() {
-  yq e '.tools.all.packages[]' ~/.config/config.yml | xargs $1 install -y
-  yq e '.tools.all.node[]' ~/.config/config.yml | xargs npm install -g
-  yes | pkg upgrade
+  yq e $tools ~/.config/config.yml | xargs $installer install -y
+  yq e $tools ~/.config/config.yml | xargs $installer npm install -g
+  yes | $installer upgrade
+  curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh | sh
+  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 }
- 
-if [ -d "$HOME/.termux" ]; then
-  pre_install pkg
-  clone_config
-  install_tools pkg
-fi
 
-mkdir docs
-curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh | sh
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-chsh -s zsh
+pre_install
+clone_config
+install_tools
+post_install
