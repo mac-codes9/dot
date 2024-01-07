e46701a28457c2e4109f7ccec969a216e2ad5bc4
Mac <maclong9@icloud.com>
2024-01-07 16:29:55 +0000

    fix(zsh/functions): removed system_update temporarily

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -25,24 +25,3 @@ system_check() {
     os="termux"
   fi
 }
-
-system_update() {
-  system_check
-  if [[ "$os" == "arch" ]]; then
-    sudo pacman -Syu
-    sudo pacman -S --needed $(yq r config.yml tools.all.packages, tools.computer.packages, tools.computer.linux)
-    npm i -g $(yq r config.yml tools.all.node, tools.computer.node)
-  elif [[ "$os" == "ubuntu" ]]; then
-    sudo apt update && sudo apt upgrade
-    sudo apt install $(yq r config.yml tools.all.packages, tools.computer.packages, tools.computer.linux)
-    npm i -g $(yq r config.yml tools.all.node, tools.computer.node)
-  elif [[ "$os" == "mac" ]]; then
-    sudo softwareupdate -i -a
-    brew install $(yq r config.yml tools.all.packages, tools.computer.packages, tools.computer)
-    npm i -g $(yq r config.yml tools.all.node, tools.computer.node)
-  elif [[ "$os" == "termux" ]]; then
-    pkg update && pkg upgrade
-    yq e '.tools.all.packages + .tools.computer.packages + .tools.computer.termux | join(" ")' config.yml | xargs pkg install
-    yq e '.tools.all.node | join(" ")' config.yml | xargs npm i -g
-  fi
-}
