4f251e7d74919e9f6cda1813384a159a9f6c040c
Mac <maclong9@icloud.com>
2024-01-08 13:36:04 +0000

    feat(install): cleaned up

--- install.sh
+++ install.sh
@@ -1,56 +1,72 @@
 #!/bin/sh
-tools=''
+installer=pkg
+install_command='install'
+tools='.tools.all.packages[] + .tools.all.zsh.packages[] + .tools.mobile.packages[] | join(" ")'
 config=~/.config/config.yml
+
+if [ -f "/etc/os-release" ]
+  source /etc/os-release
+fi
 
 if [ -d "$HOME/.termux" ]; then
   echo "Running on Termux"
-  tools='.tools.all.packages[] + .tools.all.zsh.packages[] + .tools.mobile.packages[] | join(" ")'
-  pkg in -y ruby
 elif [ "$(uname -s)" = "Darwin" ]; then
   echo "Running on macOS"
+  installer=brew
   tools='.tools.all.packages[] + .tools.all.zsh.packages[] + .tools.computer.all.packages[] + .tools.computer.mac.packages[] | join(" ")'
-elif [ -f "/etc/os-release" ] && [ "$(source /etc/os-release && echo "$ID")" = "arch" ]; then
-  echo "Runnin on Arch Linux"
+elif [ "$ID" = "arch" ]; then
+  echo "Running on Arch Linux"
+  installer=pacman
+  install_command='-Syu'
   tools='.tools.all.packages[] + .tools.all.zsh.packages[] + .tools.computer.all.packages[] + .tools.computer.linux.packages[] | join(" ")'
+elif [ "$ID" = "ubuntu" ]; then
+  echo "Running on Ubuntu"
+  installer=apt
+  tools='.tools.all.packages[] + .tools.all.zsh.packages[] + .tools.mobile.packages[] | join(" ")'
 else
   echo "Unsupported environment"
   exit
 fi
 
 pre_install() {
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+  if [ $installer = brew ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
 
-  if command -v git > /dev/null 2>&1; then
-    brew install yq
+  if [ ! -x "$(command -v git)" ]; then
+    $installer $install_command yq git
   else
-    brew install yq git
+    $installer $install_command yq
   fi
 }
 
 clone_config() {
-  cd
-  mkdir .config
-  cd .config
+  cd; mkdir .config; cd .config
   git init .
   git remote add origin https://github.com/mac-codes9/dot
   git pull origin master
-  ln -s ~/.config/vim/vimrc ~/.vimrc
-  ln -s ~/.config/zsh/zshrc ~/.zshrc
-  ln -s ~/.config/git/config ~/.gitconfig
-  ln -s ~/.config/git/msg ~/.gitmessage
+  ln -s ~/.vim/config ~/.vimrc
+  ln -s ~/.zsh/config ~/.zshrc
+  ln -s ~/.git/config ~/.gitconfig
+  ln -s ~/.git/message ~/.gitmessage
   cd; git clone https://github.com/mac-codes9/dot.wiki.git notes
 }
 
 install_tools() {
-  brew install $(yq e "$tools" "$config")
+  $installer $install_command $(yq e "$tools" "$config")
+
+  if [ ! -x "$(command -v curl)" ]; then
+    $installer $install_command curl
+  fi
+
   curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh | sh
   curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 }
 
 post_install() {
   mkdir docs
-  cd docs git clone https://github.com/mac-codes9/portfolio
+  cd docs; git clone https://github.com/mac-codes9/portfolio
   if [ "$SHELL" != "/bin/zsh" ]; then
     chsh -s zsh
   fi
